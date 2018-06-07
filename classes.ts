d03ee83bd7fef554cca526875803a59e50438fc7
Velar <Sujal Paudel>
2018-06-07 23:37:38 +0545

    Explored around class member variable

--- classes.ts
+++ classes.ts
@@ -1,23 +1,51 @@
-class Person{
-    firstName :  string;
-    lastName : string;
-    
+class Person {
+    constructor(protected firstName : string , protected lastName : string)
+    {
+
+    }
+
+
     greet()
     {
-        console.log('Hey There');
+        console.log("Hey! There");
     }
+
+    // getters and the setters:
+
+
+    getFirstName()
+    {
+        return this.firstName;
+    }
+
+    setFirstName(firstName : string)
+    {
+        this.firstName = firstName;        
+    }
+
+    getLastName()
+    {
+        return this.lastName;
+    }
+
+    setLastName(lastName : string)
+    {
+        this.lastName = lastName;        
+    }
+
 }
 
 class Programmer extends Person{
     greet()
     {
-        console.log('Hello World');
+        console.log("Hello World");
+        this.firstName 
     }
-    greet_like_normalpeople(){
+
+    greetLikeNormalPeople()
+    {
         super.greet();
     }
-
 }
-
 var aProgrammer : Person = new Programmer();
 aProgrammer.greet();
