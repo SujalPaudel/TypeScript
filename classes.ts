122c6a2f5848f517c80b37f61876abadc51b8e48
Velar <Sujal Paudel>
2018-06-08 06:20:16 +0545

    Readonly methodology

--- classes.ts
+++ classes.ts
@@ -1,51 +1,9 @@
-class Person {
-    constructor(protected firstName : string , protected lastName : string)
+class Person
+{
+    constructor(readonly name : string )
     {
-
-    }
-
-
-    greet()
-    {
-        console.log("Hey! There");
-    }
-
-    // getters and the setters:
-
-
-    getFirstName()
-    {
-        return this.firstName;
-    }
-
-    setFirstName(firstName : string)
-    {
-        this.firstName = firstName;        
-    }
-
-    getLastName()
-    {
-        return this.lastName;
-    }
-
-    setLastName(lastName : string)
-    {
-        this.lastName = lastName;        
-    }
-
-}
-
-class Programmer extends Person{
-    greet()
-    {
-        console.log("Hello World");
-        this.firstName 
-    }
-
-    greetLikeNormalPeople()
-    {
-        super.greet();
     }
 }
-var aProgrammer : Person = new Programmer();
-aProgrammer.greet();
+
+var aPerson = new Person("Kaushik");
+console.log(aPerson.name);
